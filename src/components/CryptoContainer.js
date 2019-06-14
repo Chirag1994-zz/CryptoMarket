4cd950448aa400ccd941c2553dec758d7cb49515
throughbit <DiptiVShinde>
2019-06-14 15:26:22 +0530

    searchbar

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,20 +1,50 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, StyleSheet, ScrollView, TextInput} from 'react-native';
 import FetchCoinData from './../Actions/FetchCoinData'
 import CoinCard from './CoinCard'
 import Spinner from 'react-native-loading-spinner-overlay'
+import { SearchBar, Header } from 'react-native-elements';
+
 // import console = require('console');
 
 class CryptoContainer extends Component {
-
+    constructor(props) {
+        super(props);
+    
+        // Autosuggest is a controlled component.
+        // This means that you need to provide an input value
+        // and an onChange handler that updates this value (see below).
+        // Suggestions also need to be provided to the Autosuggest,
+        // and they are initially empty because the Autosuggest is closed.
+        this.state = {
+          search:'',
+          cryptos:[]
+        }
+        this.arrayholder = [];
+        this.updateSearch = this.updateSearch.bind(this);
+      }
     componentWillMount(){
 this.props.FetchCoinData()
     }
-renderCoinCards(){
+
+renderCoinCards(data){
+    if(data){
+        return data.map((coin, index) =>
+        <CoinCard 
+            key ={index}
+            coin_name={coin.name}
+            symbol={coin.symbol}
+            price_usd = {coin.quote.USD.price}
+            percent_change_24h ={coin.quote.USD.percent_change_24h}
+            percent_change_7d = {coin.quote.USD.percent_change_7d}
+            />
+        )}
+        
+    
 const {crypto} = this.props
-// console.log(crypto)
-if(crypto.data.length > 1){
+this.arrayholder = crypto.data;
+if(crypto.data.length > 1 && this.state.search==""){
 return crypto.data.map((coin, index) =>
 <CoinCard 
     key ={index}
@@ -23,16 +53,38 @@ return crypto.data.map((coin, index) =>
     price_usd = {coin.quote.USD.price}
     percent_change_24h ={coin.quote.USD.percent_change_24h}
     percent_change_7d = {coin.quote.USD.percent_change_7d}
-
     />
 )}
+}
+clear = () => {
+    this.search.clear();
+  };
+
+// {coin.symbol.includes(this.state.search)}
+
+updateSearch(text){
+    
+      //passing the inserted text in textinput
+      const newData = this.arrayholder.filter(function(item) {
+        //applying filter for the inserted text in search bar
+        const itemData = item.name ? item.name.toUpperCase() : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      this.renderCoinCards(newData)
+      this.setState({
+        //setting the filtered newData on datasource
+        //After setting the data it will automatically re-render the view
+      
+        search:text,
+      });
 }
 
     render() {
         const {crypto} = this.props
         if(crypto.isFetching){
             return(
-                <View>
+
                     <Spinner
                         visible={crypto.isFetching}
                         textContent={"Loading..."}
@@ -40,13 +92,31 @@ return crypto.data.map((coin, index) =>
                         animation="fade"
 
                     />
-                </View>
+               
             )
         }
         return(
+            <View>
+            <Header
+  placement="left"
+  leftComponent={{ icon: 'menu', color: '#fff' }}
+  centerComponent={{ text: 'CryptoMarket', style: { color: '#fff'} }}
+  rightComponent={{ icon: 'home', color: '#fff' }}
+/>
+                         <SearchBar 
+                            
+                            onChangeText={text => this.updateSearch(text)}
+          onClear={text => this.updateSearch('')}
+          placeholder="Type Here..."
+          value={this.state.search}
+                             />
+
+                           
             <ScrollView contentContainerStyle={styles.contentContainerStyle}>
+        
                 {this.renderCoinCards()}
             </ScrollView>
+            </View>
         )
     }
 }
